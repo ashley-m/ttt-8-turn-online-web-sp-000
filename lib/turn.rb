06a8e4a2e64f38e42abbc0057a2f85c95eed584f
Ashley Miller <asm2fe@virginia.edu>
2019-03-11 19:28:52 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,31 @@
+def position_taken?(board, index)
+  if (board[index] == "O" || board[index] == "X")
+    return true
+  elsif (board[index] == nil || board[index] == " " || board[index] == "")
+    return false
+  end
+end
+
+def valid_move?(board, index)\
+  if(index.between?(0, 8) && board[index] == " ")
+    return true
+  else
+    return false
+  end
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(index)
+  space = index.to_i - 1
+end
+
+def move(board, space, char = "X")
+  board[space]=char
+end
