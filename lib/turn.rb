e424df9ecef784ecc0ec35d7f54fdafefc4eef93
Ashley Miller <asm2fe@virginia.edu>
2019-03-11 19:37:28 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,4 +32,12 @@ end
 
 def turn(board)
   puts "Please enter 1-9"
+  input = gets.strip
+  index = input_to_index(input)
+  if(valid_move(board, index))
+    move(board, index)
+  else
+    puts "Error, invalid move"
+    turn(board)
+  
 end
